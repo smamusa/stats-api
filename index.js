2e5d4b3e3207de1e0b77a7e30f32594c3576e929
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-09-17 17:25:56 +0200

    Update response to fit Shields.io spec

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ if (port == null || port == '') {
   port = 8000;
 }
 
+const response = {
+  schemaVersion: 1,
+  label: 'mystats-api',
+  message: 'Hello world !',
+  color: 'orange',
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(Object.values(response));
 });
 
 app.listen(port, () => {
