4730e6bfc5960816d274387b1265ad6ad891c709
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-09-17 18:25:04 +0200

    Fix message field in stackoverflow rep ep

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ async function getUser() {
   }
 }
 
-const response = {
-  schemaVersion: 1,
-  label: 'mystats-api',
-  message: 'Hello world !',
-  color: 'orange',
-};
-
 app.get('/', (req, res) => {
-  res.send(response);
+  res.send({
+    schemaVersion: 1,
+    label: 'mystats-api',
+    message: 'Hello world !',
+    color: 'orange',
+  });
 });
 
 app.get('/stackrep', (req, res) => {
   axios
     .get('https://api.stackexchange.com/users/5290070?site=stackoverflow')
     .then((result) => {
-      response.label = 'stackoverflow rep';
-      response.message = String(result.data.items[0].reputation);
-      response.color = 'yellow';
-      res.send(response);
+      res.send({
+        schemaVersion: 1,
+        label: 'stackoverflow rep',
+        message: String(result.data.items[0].reputation).toString(),
+        color: 'yellow',
+      });
     });
 });
 
