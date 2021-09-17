2f1ca803bf0403ea5fc328d632c24f34fc15575e
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-09-17 17:57:23 +0200

    Add stackoverflow proxy

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == '') {
   port = 8000;
+}
+
+async function getUser() {
+  try {
+    const response = await axios.get(
+      'https://api.stackexchange.com/users/5290070?site=stackoverflow'
+    );
+    console.log(response.data.items[0].reputation);
+    // console.log(response);
+    return response;
+  } catch (error) {
+    // console.error(error);
+  }
 }
 
 const response = {
@@ -17,6 +31,23 @@ app.get('/', (req, res) => {
   res.send(response);
 });
 
+app.get('/stackrep', (req, res) => {
+  let rep = getReputation();
+  response.label = 'stackoverflow rep';
+  response.message = rep;
+  response.color = 'yellow';
+  res.send(response);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+async function getReputation() {
+  try {
+    const response = await axios.get(
+      'https://api.stackexchange.com/users/5290070?site=stackoverflow'
+    );
+    return response.data.items[0].reputation;
+  } catch (error) {}
+}
