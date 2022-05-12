5f40c064115c8df4dfb5a5e7af371f41fe61a347
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2022-05-12 13:42:20 +0200

    Add test and deploy workflows
    Signed-off-by: Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>

--- test/utils.test.js
+++
@@ -1,9 +0,0 @@
-const { equal } = require('assert');
-const { payload } = require('../routes/api/matlab/utils');
-
-test('Should return object containing proper label and message', () => {
-  expect(payload('label', '41,159 of 109,712').label).toBe('label');
-  expect(payload('label', '41,159 of 109,712').message).toBe(
-    '41,159 of 109,712'
-  );
-});
